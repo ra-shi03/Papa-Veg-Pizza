637767cb2841f0d8982b0b74ee4209a88febcb2a
ra-shi03 <136837218+ra-shi03@users.noreply.github.com>
2026-09-17 11:52:44 +0530

    Delete Backend/test-auth.js

--- Backend/test-auth.js
+++
@@ -1,3 +0,0 @@
-import { signAccessToken } from './src/core/auth/token.util.js';
-const accessToken = signAccessToken({ userId: '60d5ecb8b392d7001f3e7943', role: 'FRANCHISE-ADMIN', franchiseId: '60d5ecb8b392d7001f3e7943' });
-console.log(accessToken);
